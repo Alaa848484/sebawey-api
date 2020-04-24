7a27ddee7500d4dced4fac2401b4379854a5d0e0
aboudzein <a.zeineddin@tuzlaapp.com>
2020-04-24 17:58:39 +0300

    Fixing Latest Commit

--- system/models.py
+++ system/models.py
@@ -1,6 +1,10 @@
 
+from datetime import datetime
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
 # Create your models here.
 
 class User(AbstractUser):
@@ -8,9 +12,25 @@ class User(AbstractUser):
     is_teacher = models.BooleanField(default=False)
 
 
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at =  models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True , blank=True)
+    is_deleted =  models.BooleanField(default=False)
+    uuid = models.UUIDField(default=uuid.uuid4 , editable=False, unique=True)
+    
+    def soft_delete(self):
+        self.deleted_at = datetime.now()
+        self.is_deleted = True    
+        self.save()
+        
+
+
 
 class Student(models.Model):
     user = models.OneToOneField(User, verbose_name="TheUser", on_delete=models.CASCADE , primary_key=True)
     full_name = models.CharField(max_length=100)
     age = models.IntegerField(verbose_name="Student Age" , null=True )
-    
