0fb80b0205236be7d31bea828205a8094d7c9686
aboudzein <a.zeineddin@tuzlaapp.com>
2020-04-24 14:44:39 +0300

    Create Student Profile

--- system/models.py
+++ system/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, verbose_name=_("User"), on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
+    age = models.IntegerField(verbose_name="Student Age" , null=True , max_length=60)
+    
