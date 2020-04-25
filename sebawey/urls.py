efd272572a59ec71bd2f6611302ebd9de9e82917
aboudzein <a.zeineddin@tuzlaapp.com>
2020-04-25 12:35:18 +0300

    Overrite User Auth Token Serializer & User Manager & Auth View

--- sebawey/urls.py
+++ sebawey/urls.py
@@ -1,3 +1,8 @@
+from django.contrib import admin
+from django.urls import path, re_path
+
+from system.views import obtain_auth_token
+
 """sebawey URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +18,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^api-token-auth/', obtain_auth_token),
 ]
--- system/models.py
+++ system/models.py
@@ -4,8 +4,7 @@ import uuid
 from datetime import datetime
 
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
-from django.contrib.auth.models import (AbstractUser, PermissionsMixin,
-                                        UserManager)
+from django.contrib.auth.models import (AbstractUser, PermissionsMixin)
 from django.core.mail import send_mail
 from django.db import models
 from django.db.models.signals import post_save
@@ -14,6 +13,39 @@ from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
 
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+
 class User(AbstractBaseUser , PermissionsMixin):
     email =  models.EmailField(_('email address') , unique=True)
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
@@ -21,7 +53,8 @@ class User(AbstractBaseUser , PermissionsMixin):
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
     is_student = models.BooleanField(default=False)
     is_teacher = models.BooleanField(default=False)
-
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     objects = UserManager()
     
     USERNAME_FIELD = 'email'
@@ -68,33 +101,6 @@ class BaseModel(models.Model):
         self.save()
 
 
-class UserManager(BaseUserManager):
-    use_in_migrations = True
-
-    def _create_user(self, email, password, **extra_fields):
-        """
-        Creates and saves a User with the given email and password.
-        """
-        if not email:
-            raise ValueError('The given email must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
-
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self._create_user(email, password, **extra_fields)
-
        
 
 
@@ -109,8 +115,14 @@ class StudentProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        StudentProfile.objects.create(user=instance)
+         if instance.is_student:
+            print('is student')
+            StudentProfile.objects.create(user=instance)
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     student_profile = StudentProfile.objects.get(user=instance.id)
+#     student_profile.save(**kwargs)
+    
+    
