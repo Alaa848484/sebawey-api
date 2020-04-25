a35f44f76a7c25984031a441ec59eb5a5f280425
aboudzein <a.zeineddin@tuzlaapp.com>
2020-04-25 09:15:29 +0300

    Extend User Manager

--- system/models.py
+++ system/models.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 from django.core.mail import send_mail
 import uuid
 from datetime import datetime
-
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
@@ -61,7 +61,36 @@ class BaseModel(models.Model):
         self.deleted_at = datetime.now()
         self.is_deleted = True    
         self.save()
-        
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+       
 
 
 
